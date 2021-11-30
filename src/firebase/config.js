06b0feac9a3e4e10b10e4c9e3561f37d1cde6557
Matt <mattcharlesh@gmail.com>
2021-11-30 09:02:16 -0500

    added firestore

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAPs757jAbfQ31JmcRa--KeFnsyRdguek4',
+  authDomain: 'moneyapp-ae2c0.firebaseapp.com',
+  projectId: 'moneyapp-ae2c0',
+  storageBucket: 'moneyapp-ae2c0.appspot.com',
+  messagingSenderId: '937188562356',
+  appId: '1:937188562356:web:ca70e49c195521b8d98ee1',
+}
