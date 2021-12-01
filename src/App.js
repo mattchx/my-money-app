63c0df8e98acd34f2ad20c5343a382cecab8ee90
Matt <mattcharlesh@gmail.com>
2021-12-01 11:13:28 -0500

    Persist user auth state when page refreshes

--- src/App.js
+++ src/App.js
@@ -1,30 +1,34 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 // pages & components
 import Home from './pages/home/Home'
 import Login from './pages/login/Login'
 import Signup from './pages/signup/Signup'
 import Navbar from './components/Navbar'
+import { useAuthContext } from './hooks/useAuthContext'
 
 function App() {
+  const { authIsReady } = useAuthContext()
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/signup">
-            <Signup />
-          </Route>
-        </Switch>
-      </BrowserRouter>
+    <div className='App'>
+      {authIsReady && (
+        <Router>
+          <Navbar />
+          <Switch>
+            <Route exact path='/'>
+              <Home />
+            </Route>
+            <Route path='/login'>
+              <Login />
+            </Route>
+            <Route path='/signup'>
+              <Signup />
+            </Route>
+          </Switch>
+        </Router>
+      )}
     </div>
-  );
+  )
 }
 
 export default App
